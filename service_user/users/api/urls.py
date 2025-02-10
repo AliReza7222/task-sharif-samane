4bcc05b7d4895dee9b6d15486eafcbe04f8304e5
AliReza7222 <ali.r.salehi99@gmail.com>
2025-02-11 00:47:51 +0330

    fix problem in users url

--- service_user/users/api/urls.py
+++ service_user/users/api/urls.py
@@ -5,5 +5,9 @@ from .views import UserCreateAPIView, UserRetrieveAPIView
 app_name = "users_api"
 urlpatterns = [
     path("users/create/", UserCreateAPIView.as_view(), name="user-create"),
-    path("users/<str:id>/", UserRetrieveAPIView.as_view(), name="get-user"),
+    path(
+        "users/<str:user_id>/",
+        UserRetrieveAPIView.as_view(),
+        name="get-user",
+    ),
 ]
